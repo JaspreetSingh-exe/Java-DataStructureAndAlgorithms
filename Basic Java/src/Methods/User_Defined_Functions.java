5a9cd8f7f9aea53843242da771ab8b550ae453c5
JaspreetSingh-exe <jaspreetsingh01110@gmail.com>
2025-02-08 23:15:29 +0530

    Basic Java File(User-Defined Functions) Commit

---
+++ Basic Java/src/Methods/User_Defined_Functions.java
@@ -0,0 +1,9 @@
+package Methods;
+
+public class User_Defined_Functions {
+
+    public static void main(String[] args) {
+
+        System.out.println(" File for user defined functions");
+    }
+}
